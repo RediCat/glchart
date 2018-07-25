4788dbf20c87510d33bbff99682a56be1fcad989
cvelez <christian.velez@redcatpropware.com>
2018-07-24 22:02:21 -0400

    Added skeleton of Chart class using prototype.

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
-const Vector3 = THREE.Vector3;
-const Vector2 = THREE.Vector2;
+var Vector3 = THREE.Vector3;
+var Vector2 = THREE.Vector2;
 
 main();
 
@@ -31,43 +31,16 @@ main();
  * mobile and desktop.
  */
 function main() {
-  let scope = {
+  let options = {
     size: new Vector2(800, 200),
-    zBounds: new Vector2(1, 1000)
+    cameraBounds: new Vector2(1, 1000)
   };
 
-  let scene = scope.scene = new THREE.Scene();
-  let renderer = scope.renderer = createRenderer(scope);
-  let camera = scope.camera = createCamera(scope);
-  let line = scope.line = createLine();
+  let randomData = createRandomData(100000, 100);
+  let chart = new WebGlChart(options);
+  chart.addDataset(randomData);
 
-  // render the chart
-  scene.add(line);
-  renderer.render(scene, camera);
-
-  setupGestures(scope);
-}
-
-function createRenderer(scope) {
-
-  let renderer = new THREE.WebGLRenderer();
-
-  renderer.setSize(scope.size.x, scope.size.y);
-  renderer.setPixelRatio(window.devicePixelRatio);
-
-  document.body.appendChild(renderer.domElement);
-
-  return renderer;
-}
-
-function createCamera(scope) {
-
-  let camera = new THREE.OrthographicCamera(0, scope.size.x, 0, scope.size.y, scope.zBounds.x, scope.zBounds.y);
-
-  camera.position.set(0, 0, 1);
-  camera.lookAt(new Vector3(0, 0, 0));
-
-  return camera;
+  document.body.appendChild(chart.domElement);
 }
 
 function createRandomData(size, max) {
@@ -81,27 +54,4 @@ function createRandomData(size, max) {
   }
 
   return data;
-}
-
-function createLine() {
-
-  let geometry = new THREE.Geometry();
-
-  _.forEach(createRandomData(100000, 100), (point) => {
-    geometry.vertices.push(new Vector3(point.x, point.y, 0));
-  });
-
-  let material = new THREE.LineBasicMaterial({ color: 0x0000ff });
-
-  return new THREE.Line(geometry, material);
-}
-
-function setupGestures(scope) {
-  let hammer = scope.hammer = new Hammer(scope.renderer.domElement);
-  hammer.on('panright panleft', (ev) => pan(scope, ev.deltaX));
-}
-
-function pan(scope, deltaX) {
-  scope.camera.position.x -= deltaX * 0.1;
-  scope.renderer.render(scope.scene, scope.camera);
 }
---
+++ public/js/webgl-chart/webgl-chart.js
@@ -0,0 +1,83 @@
+var Vector3 = THREE.Vector3;
+var Vector2 = THREE.Vector2;
+
+class WebGlChart {
+  constructor(options)
+  {
+    if (window._ === undefined || window._ === null) {
+      throw 'lodash appears to be missing.'
+    }
+
+    if (window.Hammer === undefined || window.Hammer === null) {
+      throw 'Hammer.js appears to be missing.'
+    }
+
+    this.options = options;
+    this.scene = new THREE.Scene();
+    this.datasetLines = [];
+
+    this._createRenderer();
+    this._createCamera();
+    this._setupGestures();
+  }
+
+  _createRenderer()
+  {
+    this.renderer = new THREE.WebGLRenderer();
+
+    this.renderer.setSize(this.options.size.x, this.options.size.y);
+    this.renderer.setPixelRatio(_.get(this.options, 'pixelRatio', window.devicePixelRatio));
+    this.domElement = this.renderer.domElement;
+  }
+
+  _createCamera()
+  {
+    let size = this.options.size, cameraBounds = this.options.cameraBounds;
+    this.camera = new THREE.OrthographicCamera(0, size.x, 0, size.y, cameraBounds.x, cameraBounds.y);
+
+    this.camera.position.set(0, 0, 1);
+    this.camera.lookAt(new Vector3(0, 0, 0));
+  }
+
+  _setupGestures()
+  {
+    this.hammer = new Hammer(this.domElement);
+    this.hammer.on('panright panleft', (ev) => this._hammerPanHandler(ev));
+  }
+
+  _hammerPanHandler(ev)
+  {
+    this.camera.position.x -= ev.deltaX * 0.1;
+    this._render();
+  }
+
+  /**
+   * This stub is used for allowing auto rendering when adding datasets, changing datasets, etc. This
+   * is used when the user wants finer grained control over the when we render the charts.
+   * @private
+   */
+  _render()
+  {
+    this.render();
+  }
+
+  addDataset(dataset)
+  {
+    let geometry = new THREE.Geometry();
+    _.forEach(dataset, (point) => {
+      geometry.vertices.push(new Vector3(point.x, point.y, 0));
+    });
+
+    let material = new THREE.LineBasicMaterial({ color: 0x0000ff });
+    let line = new THREE.Line(geometry, material);
+
+    this.scene.add(line);
+    this.datasetLines.push(line);
+    this._render();
+  }
+
+  render()
+  {
+    this.renderer.render(this.scene, this.camera);
+  }
+}
